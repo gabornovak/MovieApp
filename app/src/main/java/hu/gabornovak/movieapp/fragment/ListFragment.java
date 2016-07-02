d277865acfa3510857312f34c54761769fcf79b4
Novak Gabor <novakgabesz@gmail.com>
2016-07-02 15:16:38 +0200

    SwipeRefresh layout added.

--- app/src/main/java/hu/gabornovak/movieapp/fragment/ListFragment.java
+++ app/src/main/java/hu/gabornovak/movieapp/fragment/ListFragment.java
@@ -2,6 +2,7 @@ package hu.gabornovak.movieapp.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,20 +16,28 @@ public abstract class ListFragment extends Fragment {
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
     private TextView messageText;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public ListFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_movies_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.list);
         progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         messageText = (TextView) view.findViewById(R.id.message);
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefresh);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadList();
+            }
+        });
 
         showProgress();
-
         loadList();
 
         return view;
@@ -50,6 +59,7 @@ public abstract class ListFragment extends Fragment {
         recyclerView.setVisibility(View.GONE);
         progressBar.setVisibility(View.VISIBLE);
         messageText.setVisibility(View.GONE);
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     protected void showErrorMessage(String msg) {
@@ -57,11 +67,13 @@ public abstract class ListFragment extends Fragment {
         recyclerView.setVisibility(View.GONE);
         progressBar.setVisibility(View.GONE);
         messageText.setVisibility(View.VISIBLE);
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     protected void showList() {
         recyclerView.setVisibility(View.VISIBLE);
         progressBar.setVisibility(View.GONE);
         messageText.setVisibility(View.GONE);
+        swipeRefreshLayout.setRefreshing(false);
     }
 }
